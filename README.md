5fb1f8f3cf81a5c754e40f43da84fcfb605f8351
stephantul <stephan.tulkens@uantwerpen.be>
2018-04-05 12:15:34 +0200

    Update README and dictfeaturizer

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Currently, the repository contains the supplementary materials from the paper: "
 
 ## license
 
-The dictionaries in this repository are available under a [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) License. 
+The dictionaries in this repository are available under a [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) License.
 If you use the dictionaries in your work, please cite:
 
 ```
@@ -19,6 +19,25 @@ If you use the dictionaries in your work, please cite:
 }
 ```
 
+Note that we expanded the TA-COS submission into a journal paper, which was published in the [CLIN Journal](http://www.clinjournal.org/sites/clinjournal.org/files/Tulkens2016.pdf).
+
+If you use the dictionary expansion techniques from this paper, please also consider citing it:
+
+```
+@article{tulkens2016automated,
+  title={The automated detection of racist discourse in dutch social media},
+  author={Tulkens, St{\'e}phan and Hilte, Lisa and Lodewyckx, Elise and Verhoeven, Ben and Daelemans, Walter},
+  journal={Computational Linguistics in the Netherlands Journal},
+  volume={6},
+  number={1},
+  pages={3--20},
+  year={2016}
+}
+```
+
 ## usage
 
-The dictionaries are in .csv format. The first word of each line is the category name, while the other words are words in that category. Included is a python (2.7 & 3.x) script which reads in the dictionaries and outputs relative frequencies. Can be used for similar dictionaries, such as the LIWC dictionaries.
+The dictionaries are in .csv format.
+The first word of each line is the category name, while the other words are the words in that category.
+Included is a python (2.7 & 3.x) script which reads in the dictionaries and outputs relative frequencies.
+It can be used for similar dictionaries, such as the LIWC dictionaries.
--- dictfeaturizer.py
+++ dictfeaturizer.py
@@ -1,3 +1,4 @@
+"""A featurizer for dictionaries."""
 from __future__ import division
 from __future__ import print_function
 
@@ -8,46 +9,61 @@ from collections import Counter
 
 
 class DictFeaturizer(object):
+    """
+    Featurize text by looking up words in dictionary categories.
 
-    def __init__(self, path, rel=True):
-        """
-        The dictfeaturizer featurizes a text by looking up whether specific words in the text occur in dictionaries.
-        The dictionaries should be built in such a way that words that are semantically or functionally similar are
-        grouped.
+    Usually, the dictionary categories are built in such a way that
+    semantically or functionally similar words are grouped together.
 
-        :param path: The path to the dictionary file.
-        :param rel: Whether the frequencies returned by the object should be relative or absolute,
-        :return:
-        """
+    A good example of an application of such a Featurizer is the counting of
+    swear words in a text for the detection of toxic behavior on social media.
+    When naively counting these words, without grouping them beforehand, a
+    machine learner might not discover that swear words are similar to each
+    other.
 
+    Parameters
+    ----------
+    dictionary : dict
+        The dictionary to use. Each dictionary should have a string as key,
+        and a list of words as value. These lists of words may contain
+        wilcards in the form of '*' and '+'. These conform to their meanings in
+        regular expressions, i.e., '*' implies that the wildcard may occur,
+        while the '+' implies that it should occur.
+    relative : bool
+        Whether to return absolute or relative frequencies.
+
+    Attributes
+    ----------
+    dict : dict
+        A dictionary of tuples, the first item of each tuple contains a list
+        of normal words, while the second item of each tuple is a list of words
+        with wildcards.
+
+    """
+
+    def __init__(self, dictionary, relative=True):
+        """Initialize the DictFeaturizer."""
         self.dict = {}
-        self.rel = rel
+        self.rel = relative
         matcher = re.compile(r'([\*\+])')
 
-        with open(path, "r", encoding='utf-8') as f:
-            for line in f:
-                line = line.lower().strip().split(",")
-                key, words = line[0], set(line[1:])
+        for key, words in dictionary.items():
 
-                normal = {x for x in words if not matcher.findall(x)}
-                regexstring = "|".join([matcher.sub(r'\\w\1', x) for x in words if x not in normal])
+            normal = {x for x in words if not matcher.findall(x)}
+            regexstring = "|".join([matcher.sub(r'\\w\1', x)
+                                    for x in words if x not in normal])
 
-                if regexstring:
-                    wildcards = re.compile(regexstring)
-                else:
-                    wildcards = None
+            if regexstring:
+                wildcards = re.compile(regexstring)
+            else:
+                wildcards = None
 
-                self.dict[key] = (normal, wildcards)
+            self.dict[key] = (normal, wildcards)
 
-    def featurize(self, tokens):
-        """
-        :param: tokens: a list of tokens.
-        :type: tokens: list
-        :return: a frequency dictionary for each category in the dictionary.
-        :type: return: dict
-        """
-
-        # Make frequency dictionary of the text to diminish number of runs in further for loop
+    def transform(self, tokens):
+        """Featurize a list of tokens."""
+        # Make frequency dictionary of the text to diminish number
+        # of runs in further for loop
         freq_dict = Counter(tokens)
         features = dict()
 
@@ -59,9 +75,40 @@ class DictFeaturizer(object):
 
             features[key] = sum([freq_dict[k] for k in normal & keys])
             if wildcards:
-                features[key] += sum([freq_dict[k] for k in keys - normal if wildcards.match(k)])
-            
+                features[key] += sum([freq_dict[k] for k in keys - normal
+                                      if wildcards.match(k)])
+
         if self.rel:
             return {k: v / len(tokens) for k, v in features.items()}
         else:
             return features
+
+    @staticmethod
+    def load(path, relative=True):
+        """
+        Load a dictionary from a .csv.
+
+        The first word of each line is the category name, the other words are
+        items for that category.
+
+        Parameters
+        ----------
+        path : str
+            The path to the .csv
+        relative : bool
+            Whether to return relative or absolute frequencies.
+
+        Returns
+        -------
+        d : DictFeaturizer
+            An initialized dictfeaturizer
+
+        """
+        d = {}
+        with open(path, "r", encoding='utf-8') as f:
+            for line in f:
+                line = line.lower().strip().split(",")
+                key, words = line[0], set(line[1:])
+                d[key] = words
+
+        return DictFeaturizer(d, relative=relative)
